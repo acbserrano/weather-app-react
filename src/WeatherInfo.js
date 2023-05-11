b73666f5685ad463cfb139910c00c8e013bfb7b7
acbserrano <121968671+acbserrano@users.noreply.github.com>
2023-05-11 22:35:28 +0100

    created the unit conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./WeatherApp.css";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -11,19 +12,28 @@ export default function WeatherInfo(props) {
         <div className="col-5 text-end align-middle">
           <h1>{props.data.city}</h1>
           <h2>
-            {" "}
             <FormattedDate date={props.data.date} />
           </h2>
         </div>
-        <div className="col-2 text-center align-middle">
+        <div className="col-2 text-center align-middle mt-3">
           <WeatherIcon code={props.data.icon} alt={props.data.description} />
         </div>
         <div className="col-5">
           <ul>
-            <li> {props.data.description}</li>
-            <li>Temperature: {props.data.temperature}ºC</li>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind}km/h</li>
+            <li className="text-capitalize">{props.data.description}</li>
+            <li>
+              <WeatherTemperature
+                celsius={props.data.temperature}
+                className="d-inline"
+              />
+            </li>
+
+            <li>
+              Humidity: <strong>{props.data.humidity}%</strong>
+            </li>
+            <li>
+              Wind: <strong>{props.data.wind}km/h</strong>
+            </li>
           </ul>
         </div>
       </div>
