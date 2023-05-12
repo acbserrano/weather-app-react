6ef7ab13520e9680f539f356fc5e769d87e60794
acbserrano <121968671+acbserrano@users.noreply.github.com>
2023-05-12 11:40:17 +0100

    included weather forecast input

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#000350"
-      size={64}
+      size={props.size}
       animate={true}
     />
   );
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -4,38 +4,48 @@ import "./WeatherApp.css";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
+import WeatherForecast from "./WeatherForecast";
 
 export default function WeatherInfo(props) {
   return (
-    <div className="Weather">
-      <div className="row gridInfo">
-        <div className="col-5 text-end align-middle">
-          <h1>{props.data.city}</h1>
-          <h2>
-            <FormattedDate date={props.data.date} />
-          </h2>
-        </div>
-        <div className="col-2 text-center align-middle mt-3">
-          <WeatherIcon code={props.data.icon} alt={props.data.description} />
-        </div>
-        <div className="col-5">
-          <ul>
-            <li className="text-capitalize">{props.data.description}</li>
-            <li>
-              <WeatherTemperature
-                celsius={props.data.temperature}
-                className="d-inline"
-              />
-            </li>
+    <div>
+      <div className="Weather">
+        <div className="row gridInfo">
+          <div className="col-5 text-end align-middle">
+            <h1>{props.data.city}</h1>
+            <h2>
+              <FormattedDate date={props.data.date} />
+            </h2>
+          </div>
+          <div className="col-2 text-center align-middle mt-3">
+            <WeatherIcon
+              code={props.data.icon}
+              size={65}
+              alt={props.data.description}
+            />
+          </div>
+          <div className="col-5">
+            <ul>
+              <li className="text-capitalize">{props.data.description}</li>
+              <li>
+                <WeatherTemperature
+                  celsius={props.data.temperature}
+                  className="d-inline"
+                />
+              </li>
 
-            <li>
-              Humidity: <strong>{props.data.humidity}%</strong>
-            </li>
-            <li>
-              Wind: <strong>{props.data.wind}km/h</strong>
-            </li>
-          </ul>
+              <li>
+                Humidity: <strong>{props.data.humidity}%</strong>
+              </li>
+              <li>
+                Wind: <strong>{props.data.wind}km/h</strong>
+              </li>
+            </ul>
+          </div>
         </div>
+      </div>
+      <div className="WeatherForecast-layout">
+        <WeatherForecast />
       </div>
     </div>
   );
